6139d957bbda29dc0a1e024e44cb4b019baeacd0
Steven Bull <stevenwbulljr@gmail.com>
2019-08-16 13:35:57 -0500

    added STORE variable to javascript

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const STORE = [
+  {
+    question: 'Water is needed to sustain human life. What is the chemical formula for water?',
+    answers: {
+      a: 'H2O',
+      b: 'H2O2',
+      c: 'HO',
+      d: 'H3O+'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Which of these elements is the building block of life on earth?',
+    answers: {
+      a: 'Silicon',
+      b: 'Nitrogen',
+      c: 'Carbon',
+      d: 'Potassium'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'What is the main element in air?',
+    answers: {
+      a: 'Carbon',
+      b: 'Nitrogen',
+      c: 'Oxygen',
+      d: 'Helium'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: 'The symbol Ag stands for which element?',
+    answers: {
+      a: 'Agrillium',
+      b: 'Gallium',
+      c: 'Gold',
+      d: 'Silver'
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: 'Which of these elements react violently when exposed to water?',
+    answers: {
+      a: 'Gold',
+      b: 'Sodium',
+      c: 'Cesium',
+      d: 'Helium'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Which of the following elements is considered a noble gas?',
+    answers: {
+      a: 'Neon',
+      b: 'Oxygen',
+      c: 'Hydrogen',
+      d: 'Nitrogen'
+    },
+    correctAnswer: 'a'
+  }
+];
